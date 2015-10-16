8b7b27d7bcbcc452f0d9f9095454591e1f1def3e
ShermanDrake <shermandrake@comcast.net>
2015-10-16 10:23:25 -0600

    Friday commit

--- onevote.js
+++ onevote.js
@@ -18,20 +18,3 @@ $scope.flip = function() {
 
 }])
 
-// ng-model
-
-// <div id="beach-toys">
-  
-//   <div class="beach-toy" ng-class="{ 'selected-toy' : toy.selected }" ng-repeat="(myIndex, toy) in beachToys">
-//     <!-- Get Values of keys in object -->
-//     <!-- <p ng-repeat="(key, val) in toy">{{key}}</p> -->
-//     {{toy.name}} - {{$index}}
-//     <ul>
-//       <li ng-click="changeBackground($index, myIndex)" ng-repeat="color in toy.colors track by $index">{{color}} - {{$index}}</li>
-//     </ul>
-    
-
-//   </div>  
-
-
-// </div>
